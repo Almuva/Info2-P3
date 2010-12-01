7f1dc63edffb36636d0736c7cf2b12f08adebb38
Eduard Castany <eduard.castany@gmail.com>
2010-12-01 16:13:23 +0100

    funcio compara a partir de coordenades

--- inc/transfer.h
+++ inc/transfer.h
@@ -4,8 +4,11 @@
 
 #include <iostream> 
 #include <vector>
+#include <map>
 #include "seam_finding.h"
 #include "Imagen.h"
 
 void transferTexture(Imagen& AR,Imagen& AG,Imagen& AB,Imagen& BR,Imagen& BG,Imagen& BB,unsigned tam_Bi);
+double compara(Imagen& A,unsigned iniciAx,unsigned iniciAy,Imagen& B,
+	unsigned iniciBx,unsigned iniciBy,Imagen& Result);
 #endif
--- src/transfer.cpp
+++ src/transfer.cpp
@@ -13,5 +13,21 @@ void transferTexture(Imagen& AR,Imagen& AG,Imagen& AB,Imagen& BR,Imagen& BG,Imag
 	imagenAuxuliar=BG; imagenAuxuliar*=0.59; Luminancia2+=imagenAuxuliar;
 	imagenAuxuliar=BB; imagenAuxuliar*=0.11; Luminancia2+=imagenAuxuliar;
 	
+	compara(Luminancia1,0,0,Luminancia2,0,0,BG);
+}
+
+double compara(Imagen& A,unsigned iniciAx,unsigned iniciAy,Imagen& B,
+	unsigned iniciBx,unsigned iniciBy,Imagen& Result)
+{
+	double aux,totalEnerg=0,midaI=Result.fils(),midaJ=Result.cols();
 	
+	for(unsigned int i=0;i<midaI;i++)
+		for(unsigned int j=0;j<midaJ;j++)
+		{	
+			aux=A(i+iniciAx,j+iniciAy)-B(i+iniciBx,j+iniciBy);
+			aux*=aux;
+			Result(i,j)=aux;
+			totalEnerg+=aux*aux;
+		}
+	return totalEnerg;
 }
